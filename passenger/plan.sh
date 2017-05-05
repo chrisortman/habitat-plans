ff4fff28247d91bdea054e8a6219ea448fa65cb8
Chris Ortman <chris-ortman@uiowa.edu>
2017-05-05 10:19:23 -0500

    Initial version of my plans

---
+++ passenger/plan.sh
@@ -0,0 +1,148 @@
+# This file is the heart of your application's habitat.
+# See full docs at https://www.habitat.sh/docs/reference/plan-syntax/
+
+# Required.
+# Sets the name of the package. This will be used in along with `pkg_origin`,
+# and `pkg_version` to define the fully-qualified package name, which determines
+# where the package is installed to on disk, how it is referred to in package
+# metadata, and so on.
+pkg_name=passenger
+
+# Required unless overridden by the `HAB_ORIGIN` environment variable.
+# The origin is used to denote a particular upstream of a package.
+pkg_origin=chrisortman
+
+# Required.
+# Sets the version of the package.
+pkg_version="5.1.2"
+
+# Optional.
+# The name and email address of the package maintainer.
+# pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+
+# Optional.
+# An array of valid software licenses that relate to this package.
+# Please choose a license from http://spdx.org/licenses/
+# pkg_license=('Apache-2.0')
+
+# Required.
+# A URL that specifies where to download the source from. Any valid wget url
+# will work. Typically, the relative path for the URL is partially constructed
+# from the pkg_name and pkg_version values; however, this convention is not
+# required.
+pkg_source="http://s3.amazonaws.com/phusion-passenger/releases/passenger-${pkg_version}.tar.gz"
+
+# Optional.
+# The resulting filename for the download, typically constructed from the
+# pkg_name and pkg_version values.
+# pkg_filename="${pkg_name}-${pkg_version}.tar.gz"
+
+# Required if a valid URL is provided for pkg_source or unless do_verify() is overridden.
+# The value for pkg_shasum is a sha-256 sum of the downloaded pkg_source. If you
+# do not have the checksum, you can easily generate it by downloading the source
+# and using the sha256sum or gsha256sum tools. Also, if you do not have
+# do_verify() overridden, and you do not have the correct sha-256 sum, then the
+# expected value will be shown in the build output of your package.
+pkg_shasum="7fb03a54650ef5e508895c9e45bc2d8151f6c4811ea6797e81f017fedddfdbab"
+
+# Optional.
+# An array of package dependencies needed at runtime. You can refer to packages
+# at three levels of specificity: `origin/package`, `origin/package/version`, or
+# `origin/package/version/release`.
+ pkg_deps=(
+   core/glibc
+   core/coreutils
+   core/curl
+   core/openssl
+   core/zlib
+   chrisortman/ruby
+)
+
+# Optional.
+# An array of the package dependencies needed only at build time.
+pkg_build_deps=(
+  core/coreutils
+  core/make
+  core/gcc
+  chrisortman/ruby
+  core/patchelf
+)
+
+pkg_lib_dirs=(lib)
+pkg_include_dirs=(include)
+pkg_bin_dirs=(bin)
+pkg_interpreters=(bin/bash)
+
+do_prepare() {
+  fix_interpreter "bin/*" core/coreutils bin/env
+}
+
+# The default implementation is to update the prefix path for the configure
+# script to use $pkg_prefix and then run make to compile the downloaded source.
+# This means the script in the default implementation does
+# ./configure --prefix=$pkg_prefix && make. You should override this behavior
+# if you have additional configuration changes to make or other software to
+# build and install as part of building your package.
+do_build() {
+  export EXTRA_CFLAGS="${CPPFLAGS} ${CFLAGS}"
+  export EXTRA_CXXFLAGS="${CPPFLAGS} ${CFLAGS}"
+  export EXTRA_LD_FLAGS="${LD_RUN_PATH}"
+
+  build_line "Compiling passenger agent"
+  bin/passenger-config compile-agent --auto
+  # curl -f -L -o agent-x86_64-linux.tar.gz -s -S --connect-timeout 30 --speed-time 30 --speed-limit 1 https://oss-binaries.phusionpassenger.com/binaries/passenger/by_release/${pkg_version}/agent-x86_64-linux.tar.gz
+
+  # mkdir -p buildout/support-binaries
+  # tar -xzf agent-x86_64-linux.tar.gz -C buildout/support-binaries
+  # patchelf --interpreter "$(pkg_path_for glibc)/lib/ld-linux-x86-64.so.1" --set-rpath "$LD_RUN_PATH" buildout/support-binaries/PassengerAgent
+  # curl -f -L -o nginx-1.10.2-x86_64-linux.tar.gz -s -S --connect-timeout 30 --speed-time 30 --speed-limit 1 https://oss-binaries.phusionpassenger.com/binaries/passenger/by_release/${pkg_version}/nginx-1.10.2-x86_64-linux.tar.gz
+
+  # curl -f -L -o rubyext-ruby-2.1.9-x86_64-linux.tar.gz -s -S --connect-timeout 30 --speed-time 30 --speed-limit 1 https://oss-binaries.phusionpassenger.com/binaries/passenger/by_release/${pkg_version}/rubyext-ruby-2.1.9-x86_64-linux.tar.gz
+
+  # rake nginx
+  # bin/passenger-config download-agent --force --suppress-binary-unusable-message
+  #
+  # build_line "Installing passenger runtime"
+  # bin/passenger-config install-standalone-runtime --auto
+
+  $(hab pkg path chrisortman/ruby)/bin/ruby src/ruby_native_extension/extconf.rb
+}
+
+# The default implementation runs nothing during post-compile. An example of a
+# command you might use in this callback is make test. To use this callback, two
+# conditions must be true. A) do_check() function has been declared, B) DO_CHECK
+# environment variable exists and set to true, env DO_CHECK=true.
+do_check() {
+  bin/passenger-config validate-install
+}
+
+# The default implementation is to run make install on the source files and
+# place the compiled binaries or libraries in HAB_CACHE_SRC_PATH/$pkg_dirname,
+# which resolves to a path like /hab/cache/src/packagename-version/. It uses
+# this location because of do_build() using the --prefix option when calling the
+# configure script. You should override this behavior if you need to perform
+# custom installation steps, such as copying files from HAB_CACHE_SRC_PATH to
+# specific directories in your package, or installing pre-built binaries into
+# your package.
+do_install() {
+  echo "Copying current files to ${pkg_prefix}"
+  cp -a . "${pkg_prefix}"
+}
+
+# The default implementation is to strip any binaries in $pkg_prefix of their
+# debugging symbols. You should override this behavior if you want to change
+# how the binaries are stripped, which additional binaries located in
+# subdirectories might also need to be stripped, or whether you do not want the
+# binaries stripped at all.
+do_strip() {
+#  do_default_strip
+return 0
+}
+
+# There is no default implementation of this callback. This is called after the
+# package has been built and installed. You can use this callback to remove any
+# temporary files or perform other post-install clean-up actions.
+do_end() {
+  return 0
+}
+
---
+++ ruby-web/plan.sh
@@ -0,0 +1,54 @@
+pkg_name=ruby
+pkg_origin=chrisortman
+pkg_version=2.1.5
+pkg_description="A dynamic, open source programming language with a focus on \
+  simplicity and productivity. It has an elegant syntax that is natural to \
+  read and easy to write."
+pkg_license=("Ruby")
+pkg_maintainer="Chris Ortman <chrisortman@gmail.com>"
+pkg_source=https://cache.ruby-lang.org/pub/ruby/ruby-${pkg_version}.tar.gz
+pkg_upstream_url=https://www.ruby-lang.org/en/
+pkg_filename=ruby-${pkg_version}.tar.gz
+pkg_shasum=4305cc6ceb094df55210d83548dcbeb5117d74eea25196a9b14fa268d354b100
+pkg_deps=(core/glibc core/ncurses core/zlib core/openssl core/libyaml core/libffi)
+pkg_build_deps=(core/coreutils core/diffutils core/patch core/make core/gcc core/sed)
+pkg_lib_dirs=(lib)
+pkg_include_dirs=(include)
+pkg_bin_dirs=(bin)
+pkg_interpreters=(bin/ruby)
+
+do_build() {
+    CFLAGS="${CFLAGS} -O3 -g -pipe"
+    #patch -p1 -i "$PLAN_CONTEXT/patches/ruby-2_1_3-no-mkmf.patch"
+
+    # Resolves issue for older versions of RubyGems which require new trust
+    # authority SSL certificate required as of 2016-10-06.
+    #
+    # Most likely the next Ruby release will resolve this issue as the vendored
+    # version of RubyGems should be newer.
+    #
+    # For more details see:
+    # http://guides.rubygems.org/ssl-certificate-update/#manual-solution-to-ssl-issue
+    cp -v "$PLAN_CONTEXT/GlobalSignRootCA.pem" lib/rubygems/ssl_certs/
+
+    ./configure "--prefix=$pkg_prefix" \
+                --enable-shared \
+                --disable-install-doc \
+                "--with-openssl-dir=$(_resolve_dependency core/openssl)" \
+                "--with-libyaml-dir=$(_resolve_dependency core/libyaml)"
+    make
+}
+
+do_install() {
+  do_default_install
+  gem update --system --no-document
+  gem install rb-readline --no-document
+  gem install bundler --no-ri --no-rdoc
+  gem install passenger -v "5.1.2" --no-ri --no-rdoc
+
+
+}
+
+do_check() {
+  make test
+}
